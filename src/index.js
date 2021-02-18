930897eed2f8321d9916e14e736c46daaac3beae
kishan kumar a <kishankumar12324@gmail.com>
2021-02-18 12:08:51 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const session_secret = "newton";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = express();
+app.use(express.json()); 
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
+app.use(
+  session({
+    secret: session_secret,
+    cookie: { maxAge: 1*60*60*1000 }
+  })
+); 
+const db = mongoose.createConnection("mongodb://localhost:27017/services",{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const serviceSchema=new mongoose.Schema({
+  id:Number,
+  image:String,
+  title:String,
+  description:String,
+  userId:mongoose.Schema.Types.ObjectId
+});
+
+const serviceModel=db.model("services",serviceSchema);
+
+app.get("/services", async(req,res)=>{
+  const allservices=await serviceModel.find({
+    userId:req.session.userId
+  });
+  res.send(allservices);
+});
+
+app.post("/services", async(req,res)=>{
+ try{ const service=req.body;
+  console.log(req.body)
+  service.userId=req.session.userId;
+  const newservice=new serviceModel(service);
+  await newservice.save();
+  res.send(newservice);
+ }catch(e){
+   res.sendStatus(404);
+ }
+});
+app.listen(8888);
